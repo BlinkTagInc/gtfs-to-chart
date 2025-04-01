cf21b813929c7ad4d63ad9f381dfdebd54921064
Brendan Nee <brendan@blinktag.com>
2025-04-01 11:43:06 -0700

    Dependency updates

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-
 import express from 'express';
 import logger from 'morgan';
 import slashes from 'connect-slashes';
@@ -8,61 +7,63 @@ import slashes from 'connect-slashes';
 import routes from './routes.js';
 
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // View engine setup
-app.set('views', path.join(fileURLToPath(import.meta.url), '../views'));
+app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 
+// Middleware
 app.use(logger('dev'));
-app.use(express.static(path.join(fileURLToPath(import.meta.url), '../../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(slashes());
 
+// Routes
 app.use('/', routes);
 
 // Error handlers
-
-// 404 error handler
 app.use((request, response) => {
   const error = {
     message: 'Not Found',
-    status: 404
+    status: 404,
   };
   response.status(404);
   if (request.xhr) {
-    response.send({
+    response.json({
       message: error.message,
-      error
+      error,
     });
   } else {
     response.render('error', {
       message: error.message,
-      error
+      error,
     });
   }
 });
 
 // Development error handler: will print stacktrace
 if (process.env.NODE_ENV === 'development') {
-  app.use((error, request, response) => {
+  app.use((error, request, response, next) => {
     response.status(error.status || 500);
     response.render('error', {
       message: error.message,
-      error
+      error,
     });
   });
 }
 
 // Production error handler: no stacktraces leaked to user
-app.use((error, request, response) => {
+app.use((error, request, response, next) => {
   response.status(error.status || 500);
   response.render('error', {
     message: error.message,
-    error: {}
+    error: {},
   });
 });
 
-app.set('port', process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.set('port', port);
 
-const server = app.listen(app.get('port'), () => {
+const server = app.listen(port, () => {
   console.log(`Express server listening on port ${server.address().port}`);
 });
